2f7ce6ad9a5b3c6ca8d954e34dfde70567ac308f
Maxsuel Marinho <maxsuelmarinho@gmail.com>
2018-09-28 00:40:53 -0300

    Add aircraft selection

--- js/aircraft.js
+++ js/aircraft.js
@@ -82,6 +82,14 @@ var aircraft = {
             var x = (this.x * game.gridSize) - game.offsetX - this.pixelOffsetX;
             var y = (this.y * game.gridSize) - game.offsetY - this.pixelOffsetY - this.pixelShadowHeight;
 
+            this.drawingX = x;
+            this.drawingY = y;
+
+            if (this.selected) {
+                this.drawSelection();
+                this.drawLifeBar();
+            }
+
             var colorIndex = (this.team == "blue") ? 0 : 1;
             var colorOffset = colorIndex * this.pixelHeight;
             var shadowOffset = this.pixelHeight * 2;
@@ -103,7 +111,51 @@ var aircraft = {
                 x, y + this.pixelShadowHeight,
                 this.pixelWidth, this.pixelHeight
             );
-        }
+        },
+
+        drawLifeBar: function () {
+            var x = this.drawingX;
+            var y = this.drawingY - 2 * game.lifeBarHeight;
+
+            game.foregroundContext.fillStyle = (this.lifeCode == "healthy") ?
+                game.healthBarHealthyFillColor :
+                game.healthBarDamagedFillColor;
+
+            game.foregroundContext.fillRect(
+                x, y,
+                this.pixelWidth * this.life / this.hitPoints,
+                game.lifeBarHeight
+            );
+
+            game.foregroundContext.strokeStyle = game.healthBarBorderColor;
+            game.foregroundContext.lineWidth = 1;
+            game.foregroundContext.strokeRect(
+                x, y,
+                this.pixelWidth, this.lifeBarHeight
+            );
+        },
+
+        drawSelection: function () {
+            var x = this.drawingX + this.pixelOffsetX;
+            var y = this.drawingY + this.pixelOffsetY;
+
+            game.foregroundContext.strokeStyle = game.selectionBorderColor;
+            game.foregroundContext.lineWidth = 2;
+            game.foregroundContext.beginPath();
+            game.foregroundContext.arc(x, y, this.radius, 0, Math.PI * 2, false);
+            game.foregroundContext.stroke();
+            game.foregroundContext.fillStyle = game.selectionFillColor;
+            game.foregroundContext.fill();
+
+            game.foregroundContext.beginPath();
+            game.foregroundContext.arc(x, y + this.pixelShadowHeight, 4, 0, Math.PI * 2, false);
+            game.foregroundContext.stroke();
+
+            game.foregroundContext.beginPath();
+            game.foregroundContext.moveTo(x, y);
+            game.foregroundContext.lineTo(x, y + this.pixelShadowHeight);
+            game.foregroundContext.stroke();
+        },
     },
 
     load: loadItem,
